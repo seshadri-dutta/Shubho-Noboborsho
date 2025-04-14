0a93321db051263a6a8856d452bc636a269eab01
seshadri-dutta <seshadridutta@gmail.com>
2025-04-15 02:09:21 +0530

    Bug Fix

--- src/pages/WishPage.jsx
+++ src/pages/WishPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import WishCard from '../components/WishCard';
+import WishCard from '../components/WishcardNew';
 
 const wishes = [
   {
